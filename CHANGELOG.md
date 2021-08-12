f23de2749aa095d0bf7e64121114eff6d1887161
kodipe <konrad.przydzial@gmail.com>
2021-08-12 09:49:04 +0200

    Response::file() and 0.0.4

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.4
+
+- Added: `Response::file()` method for downloading files from server
--- src/Response.php
+++ src/Response.php
@@ -47,6 +47,19 @@ class Response {
 
     return $this;
   }
+
+  function file($path) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($path).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($path));
+    flush(); // Flush system output buffer
+    readfile($path);
+    die();
+  }
 }
 
 ?>
